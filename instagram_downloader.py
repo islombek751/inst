510f471f39f613909e3d6a86cd7f6f0a15a4c5dc
Islombek Mukhtorov <tanlanganasarlar@gmail.com>
2022-06-19 13:50:58 +0300

    sasa

--- instagram_downloader.py
+++ instagram_downloader.py
@@ -44,6 +44,13 @@ app = flask.Flask(__name__)
 # Empty webserver index, return nothing, just http 200
 @app.get('/')
 def index():
+    bot.remove_webhook()
+
+    time.sleep(0.1)
+
+    # Set webhook
+    bot.set_webhook(url=WEBHOOK_HOST + WEBHOOK_URL_PATH,
+                )
     import instaloader
 
     L = instaloader.Instaloader()
@@ -142,13 +149,7 @@ def linkto(message):
         bot.send_message(message.chat.id,'Send only Instagram links!')
         
 # Remove webhook, it fails sometimes the set if there is a previous webhook
-bot.remove_webhook()
 
-time.sleep(0.1)
-
-# Set webhook
-bot.set_webhook(url=WEBHOOK_HOST + WEBHOOK_URL_PATH,
-                )
 
 # Start flask server
 app.run(host=WEBHOOK_LISTEN,
